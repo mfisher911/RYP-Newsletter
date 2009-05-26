43f091937c389488ad6cd271982f0c1896fb45b8
Mike Fisher <mfisher@csh.rit.edu>
2009-05-25 23:15:36 -0400

    * Handled a bug with no records returned by Google. * Refactored spreadsheet handling -> array of dicts logic * Added filtering of board seats to suppress the shortdesc if a longdesc   was shown. * Added job opportunity handling. * Refactored similar code for board seat longdescs and job opportunities.

--- RYP.py
+++ RYP.py
@@ -6,8 +6,15 @@ import csv
 from string import Template
 
 def no_blanks(x):
-    if not x.startswith('--,'):
+    if not x.startswith('--,') and not x.startswith('#N/A,'):
         return x
+
+def clean_spreadsheet_data(url):
+    result = []
+    data = csv.DictReader(filter(no_blanks, get_spreadsheet_data(url)))
+    for i in data:
+        result.append(i)
+    return result
 
 def get_spreadsheet_data(url):
     (host,slash,path) = url.partition('/')
@@ -23,14 +30,24 @@ def get_spreadsheet_data(url):
     return data_csv
 
 def get_board_seats():
-    return filter(no_blanks, get_spreadsheet_data("spreadsheets.google.com/pub?key=rWOj5fxWi1wQBwy8pVbajng&output=csv&gid=1"))
+    return clean_spreadsheet_data("spreadsheets.google.com/pub?key=rWOj5fxWi1wQBwy8pVbajng&output=csv&gid=1")
 
 def get_board_detail():
-    return filter(no_blanks, get_spreadsheet_data("spreadsheets.google.com/pub?key=rWOj5fxWi1wQBwy8pVbajng&output=csv&gid=3"))
+    return clean_spreadsheet_data("spreadsheets.google.com/pub?key=rWOj5fxWi1wQBwy8pVbajng&output=csv&gid=3")
 
-def display_board_seats(board_seats_csv):
+def get_job_ops():
+    return clean_spreadsheet_data("spreadsheets.google.com/pub?key=rWOj5fxWi1wQBwy8pVbajng&gid=5&output=csv")
+
+def de_dupe(detail, seats):
+    for i in detail:
+        for j in seats:
+            if i['Organization_Name'] == j['Charity']:
+                seats = [s for s in seats if s['Charity'] <> j['Charity']]
+    return seats
+
+def display_board_seats(board_seats):
     accum = ''
-    for bs in csv.DictReader(board_seats_csv):
+    for bs in board_seats:
         name = ''
         email = ''
         url = ''
@@ -63,10 +80,29 @@ def display_board_seats(board_seats_csv):
                         ])
     return "<ul>\n" + accum + "</ul>"
 
+def format_desc_html(desc):
+    return desc.replace("<", "&lt;").replace(">", "&gt;")
+
+def format_org_name(i):
+    name = "<b>" + i['Organization_Name'] + "</b>"
+    desc = format_desc_html(i['Description'])
+    desc = desc.replace(i['Organization_Name'], name, 1)
+    return desc
+    
 def display_board_detail(board_details):
     accum = ''
-    for i in csv.DictReader(board_details):
-        name = "<b>" + i['Organization_Name'] + "</b>"
-        desc = i['Description'].replace(i['Organization_Name'], name, 1)
+    for i in board_details:
+        desc = format_org_name(i)
         accum += "<li>" + desc + "</li>\n"
     return "<ul>\n" + accum + "</ul>"
+
+def display_job_ops(job_ops):
+    accum = ''
+    if len(job_ops) == 1:
+        i = job_ops[0]
+        return "<p>" + format_org_name(i) + "</p>"
+    elif len(job_ops) > 1:
+        for i in job_ops:
+            desc = format_org_name(i)
+            accum += "<li>" + desc + "</li>\n"
+        return "<ul>\n" + accum + "</ul>"
